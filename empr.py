41c5c9c379030246e5850dc6b4bd4e07852002c2
Hrishikesh Srihari <rishisrihari9@gmail.com>
2021-02-22 01:49:11 -0600

    added value and function classes and updated number class

--- empr.py
+++ empr.py
@@ -943,9 +943,10 @@ class RTResult:
 
 # VALUES ##############################
 
-class Number:
-	def __init__(self, value):
-		self.value = value
+# VALUE CLASS ##############################
+
+class Value:
+	def __init__(self):
 		self.set_pos()
 		self.set_context()
 
@@ -959,16 +960,89 @@ class Number:
 		return self
 
 	def added_to(self, other):
+		return None, self.illegal_oper(other)
+
+	def subbed_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def multed_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def dived_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def powed_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_eq(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_ne(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_lt(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_gt(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_lte(self, other):
+		return None, self.illegal_oper(other)
+
+	def get_compar_gte(self, other):
+		return None, self.illegal_oper(other)
+
+	def anded_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def ored_by(self, other):
+		return None, self.illegal_oper(other)
+
+	def notted(self):
+		return None, self.illegal_oper(other)
+
+	def execute(self, args):
+		return RTResult().failure(self.illegal_oper())
+
+	def copy(self):
+		raise Exception('No copy method defined')
+
+	def is_true(self):
+		return False
+
+	def illegal_oper(self, other=None):
+		if not other: other = self
+		return RTError(
+			self.pos_start, other.pos_end,
+			'Illegal operation',
+			self.context
+		)
+
+# NUMBER CLASS ##############################
+
+
+class Number(Value):
+	def __init__(self, value):
+		super().__init__()
+		self.value = value
+
+	def added_to(self, other):
 		if isinstance(other, Number):
 			return Number(self.value + other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def subbed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value - other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def multed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value * other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def dived_by(self, other):
 		if isinstance(other, Number):
@@ -980,42 +1054,62 @@ class Number:
 				)
 
 			return Number(self.value / other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def powed_by(self, other):
 		if isinstance(other, Number):
 			return Number(self.value ** other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_eq(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value == other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_ne(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value != other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_lt(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value < other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_gt(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value > other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_lte(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value <= other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def get_compar_gte(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value >= other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def anded_by(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value and other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def ored_by(self, other):
 		if isinstance(other, Number):
 			return Number(int(self.value or other.value)).set_context(self.context), None
+		else:
+			return None, Value.illegal_oper(self, other)
 
 	def notted(self):
 		return Number(1 if self.value == 0 else 0).set_context(self.context), None
@@ -1031,6 +1125,54 @@ class Number:
 
 	def __repr__(self):
 		return str(self.value)
+
+# FUNCTION CLASS ##############################
+
+class Function(Value):
+	def __init__(self, name, body_node, arg_names):
+		super().__init__()
+		self.name = name or "<anonymous>"
+		self.body_node = body_node
+		self.arg_names = arg_names
+
+	def execute(self, args):
+		res = RTResult()
+		interpreter = Interpreter()
+		new_context = Context(self.name, self.context, self.pos_start)
+		new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+
+		if len(args) > len(self.arg_names):
+			return res.failure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(args) - len(self.arg_names)} too many args passed into '{self.name}'",
+				self.context
+			))
+
+		if len(args) < len(self.arg_names):
+			return res.failure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(self.arg_names) - len(args)} too few args passed into '{self.name}'",
+				self.context
+			))
+
+		for i in range(len(args)):
+			arg_name = self.arg_names[i]
+			arg_value = args[i]
+			arg_value.set_context(new_context)
+			new_context.symbol_table.set(arg_name, arg_value)
+
+		value = res.register(interpreter.visit(self.body_node, new_context))
+		if res.error: return res
+		return res.success(value)
+
+	def copy(self):
+		copy = Function(self.name, self.body_node, self.arg_names)
+		copy.set_context(self.context)
+		copy.set_pos(self.pos_start, self.pos_end)
+		return copy
+
+	def __repr__(self):
+		return f"<function {self.name}>"
 
 class Context:
 	def __init__(self, display_name, parent=None, parent_entry_pos=None):
