6de4502956fd046b139e5c95a558cd0ffaf39bed
Hrishikesh Srihari <rishisrihari9@gmail.com>
2021-03-28 21:16:26 -0500

    created base functions to be used in function class

--- empr.py
+++ empr.py
@@ -1266,6 +1266,47 @@ class List(Value):
 	def __repr__(self):
 		return f'[{", ".join([str(x) for x in self.elements])}]'
 
+class BaseFunction(Value):
+	def __init__(self, name):
+		super().__init__()
+		self.name = name or "<anonymous>"
+	def generate_new_context(self):
+		new_context = Context(self.name, self.context, self.pos_start)
+		new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+		return new_context
+
+	def check_args(self, arg_names, args):
+		res = RTResult()
+
+		if len(args) > len(arg_names):
+			return res.failure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(args) - len(arg_names)} too many args passed into '{self.name}'",
+				self.context
+			))
+
+		if len(args) < len(arg_names):
+			return res.failure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(arg_names) - len(args)} too few args passed into '{self.name}'",
+				self.context
+			))
+		return res.success(None)
+
+	def populate_args(self, arg_names, args, exec_ctx):
+		for i in range(len(args)):
+    		arg_name = arg_names[i]
+      		arg_value = args[i]
+      		arg_value.set_context(exec_ctx)
+      		exec_ctx.symbol_table.set(arg_name, arg_value)
+
+	def check_and_populate_args(self, arg_names, args, exec_ctx):
+		res = RTResult()
+    	res.register(self.check_args(arg_names, args))
+    	if res.error: return res
+    	self.populate_args(arg_names, args, exec_ctx)
+    	return res.success(None)
+
 class Function(Value):
 	def __init__(self, name, body_node, arg_names):
 		super().__init__()
